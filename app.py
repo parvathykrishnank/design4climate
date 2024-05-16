68dd7a7039a8ae1283c2772d10e7877dfc8b4ff2
Parvathy Krishnan K <parvathykrishnank@gmail.com>
2024-05-16 03:32:43 +0200

    updated dashboard

--- app.py
+++ app.py
@@ -8,13 +8,73 @@ app = Flask(__name__)
 
 @app.route('/')
 def dashboard():
+
+    selected_cmu = request.args.get('CMU', 'All')
+    selected_country = request.args.get('COUNTRY', 'All')
+
+    country_to_code = {
+    'Albania': 'ALB',
+    'Armenia': 'ARM',
+    'Azerbaijan': 'AZE',
+    'Bosnia & Herzegovina': 'BIH',
+    'Croatia': 'HRV',
+    'Georgia': 'GEO',
+    'Kazakhstan': 'KAZ',
+    'Kosovo': 'XKX',
+    'Kyrgyzstan': 'KGZ',
+    'Moldova': 'MDA',
+    'Montenegro': 'MNE',
+    'North Macedonia': 'MKD',
+    'Poland': 'POL',
+    'Romania': 'ROU',
+    'Serbia': 'SRB',
+    'Tajikistan': 'TJK',
+    'Turkiye': 'TUR',
+    'Turkmenistan': 'TKM',
+    'Ukraine': 'UKR',
+    'Uzbekistan': 'UZB'
+    }
+
     ncasdf = pd.read_excel('database.xlsx')
-    return render_template('dashboard.html')
+
+    if selected_cmu != 'All':
+        ncasdf = ncasdf[ncasdf['CMU'] == selected_cmu]
+
+    if selected_country != 'All':
+        ncasdf = ncasdf[ncasdf['Country'] == selected_country]
+
+    table_df = ncasdf.copy()
+    count_df = table_df.groupby(['Country'])['Document Title'].count().reset_index()
+    count_df['Country_Code'] = count_df['Country'].apply(lambda x: country_to_code[x])
+
+    policy_type_count = table_df.groupby(['Policy type'])['Document Title'].count().reset_index()
+    policy_type_count['policy type'] = policy_type_count['Policy type'].apply(lambda x:x.replace('/',' and '))
+
+    cmu_list = ncasdf['CMU'].unique().tolist()
+    cmu_list.sort()
+
+    country_list = ncasdf['Country'].unique().tolist()
+    country_list.sort()
+
+    cmu_list.insert(0, 'All')
+    country_list.insert(0, 'All')
+
+    return render_template(
+        'dashboard.html', 
+        country_counts=count_df.to_dict(orient='records'),
+        cmu_list=cmu_list,
+        selected_cmu=selected_cmu,
+        country_list=country_list,
+        selected_country=selected_country,
+        table_data=table_df.to_dict(orient='records'),
+        total_docs = str(ncasdf['Document Title'].count()),
+        policy_type_count=list(policy_type_count.values.tolist())
+    )
 
 @app.route('/index')
 def index():
     ncasdf = pd.read_excel('database.xlsx')
-    color_status_dict = {'Adopted':'green','Work in Progress':'orange'}
+    color_status_dict = {'Adopted':'green','Draft':'yellow','Work in Progress':'orange'}
     ncasdf['color_status'] = ncasdf['Status'].apply(lambda x: color_status_dict[x])
     color_language_dict = {'English':'purple','Non-English':'violet'}
     ncasdf['color_language'] = ncasdf['Language'].apply(lambda x: color_language_dict[x])
@@ -26,7 +86,6 @@ def index():
 def record():
     if request.method == 'POST':
         ncasdf = pd.read_excel('database.xlsx')
-
         length_df = len(ncasdf)
         cmu = request.form['CMU']
         country = request.form['Country']
